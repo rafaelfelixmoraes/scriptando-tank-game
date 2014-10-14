0c5de14a22e47c9eaf2b13bc2caa0da7919c291c
erickzanardo <erickzanardoo@gmail.com>
2014-10-14 17:25:13 -0300

    Teste

--- tests/TankTest.js
+++ tests/TankTest.js
@@ -7,15 +7,15 @@ describe('Tank', function(){
         it('Deve descontar municao depois de atirar', function() {
             var tank = new Tank();
             tank.setMunicoes(1);
-            tank.dispararEm(1, 1);
+            assert.equal(true, tank.dispararEm(1, 1));
             assert.equal(0, tank.getMunicoes());
         });
 
         it('Deve ter municao para poder atirar', function() {
             var tank = new Tank();
             tank.setMunicoes(1);
-            assert.true(tank.dispararEm(1, 1)); // Descontado uma municao
-            assert.false(tank.dispararEm(1, 1));
+            assert.equal(true, tank.dispararEm(1, 1)); // Descontado uma municao
+            assert.equal(false, tank.dispararEm(1, 1));
         });
 
         it('Deve ter alcance para poder atirar', function() {
@@ -23,8 +23,8 @@ describe('Tank', function(){
             tank.setMunicoes(2);
             tank.setAlcance(10);
             tank.setPosicao(0, 0);
-            assert.true(tank.dispararEm(1, 1)); // Descontado uma municao
-            assert.false(tank.dispararEm(11, 0));
+            assert.equal(true, tank.dispararEm(1, 1)); // Descontado uma municao
+            assert.equal(false, tank.dispararEm(11, 0));
         });
 
         it('Atirar para tras', function() {
@@ -32,8 +32,38 @@ describe('Tank', function(){
             tank.setMunicoes(1);
             tank.setAlcance(10);
             tank.setPosicao(10, 0);
-            assert.true(tank.dispararEm(1, 1));
+            assert.equal(true, tank.dispararEm(1, 1));
         });
+    });
+    describe('Mover', function(){
+        it('Movimentacao', function() {
+            var tank = new Tank();
+            tank.setMovimentacao(10);
+            tank.setPosicao(0, 0);
+            assert.equal(true, tank.moverPara(0, 5));
+            assert.equal(true, tank.moverPara(5, 5));
+            assert.equal(false, tank.moverPara(0, 11));
+            assert.equal(false, tank.moverPara(5, 6));
+        });
+    });
+    describe('Dano', function() {
+        it('Aplicar', function() {        
+            // Dano = (Calibre - Blindagem) * 2
+            var tank1 = new Tank();
+            tank1.setDurabilidade(100);
+            tank1.setCalibre(90);
+            tank1.setBlindagem(80);
+                        
+            var tank2 = new Tank();
+            tank2.setDurabilidade(100);
+            tank2.setCalibre(70);
+            tank2.setBlindagem(80);
 
+            tank1.recebeDano(tank2);
+            tank2.recebeDano(tank1);
+            
+            assert.equal(100, tank1.getDurabilidade());
+            assert.equal(80, tank2.getDurabilidade());
+        });
     });
 });
